6f941782ba263704a450b6a84f5a8dc41bdc29b8
Justine Arriaga <fer123arriaga@gmail.com>
2024-07-31 22:54:34 -0600

    Initial commit

---
+++ src/test/java/com/app/devGrill/DevGrillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.devGrill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevGrillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
